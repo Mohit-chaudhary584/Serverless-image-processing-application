0e35874a8908b8ab30f5e9758cb4a9744e6807ce
Mohit-chaudhary584 <mohit7838503206@gmail.com>
2024-12-08 01:31:28 +0530

    added light and dark mode functionality

--- script.js
+++ script.js
@@ -142,3 +142,13 @@ function displayLabels(labels) {
         list.appendChild(listItem);
     });
 }
+
+document.getElementById('user').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('#user i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+    }
+});
